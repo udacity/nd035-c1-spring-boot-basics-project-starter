29d2bb66ad1d8e658afa514db2aab9a052d516a0
Ali Malik <ali.malik91@hotmail.com>
2019-12-23 08:40:23 -0500

    Starter Code

---
+++ starter/cloudstorage/src/main/resources/schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS USERS (
+  userid INT PRIMARY KEY auto_increment,
+  username VARCHAR(20),
+  salt VARCHAR,
+  password VARCHAR,
+  firstname VARCHAR(20),
+  lastname VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS NOTES (
+    noteid INT PRIMARY KEY auto_increment,
+    notetitle VARCHAR(20),
+    notedescription VARCHAR (1000),
+    userid INT,
+    foreign key (userid) references USERS(userid)
+);
+
+CREATE TABLE IF NOT EXISTS FILES (
+    fileId INT PRIMARY KEY auto_increment,
+    filename VARCHAR,
+    contenttype VARCHAR,
+    filesize VARCHAR,
+    userid INT,
+    filedata BLOB,
+    foreign key (userid) references USERS(userid)
+);
+
+CREATE TABLE IF NOT EXISTS CREDENTIALS (
+    credentialid INT PRIMARY KEY auto_increment,
+    url VARCHAR(100),
+    username VARCHAR (30),
+    key VARCHAR,
+    password VARCHAR,
+    userid INT,
+    foreign key (userid) references USERS(userid)
+);
