63996bc63f9916e965a498b92f33e81abe77a22d
Peter Zastoupil <peterzastoupil@gmail.com>
2020-02-21 11:13:18 -0600

    Updated project from using gradle to maven

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudstorageApplication.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudstorageApplication.java
@@ -1,13 +1,13 @@
-package com.udacity.jdnd.course1.cloudstorage;
+package com.udacity.jwdnd.course1.cloudstorage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudStorageApplication {
+public class CloudstorageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudStorageApplication.class, args);
+		SpringApplication.run(CloudstorageApplication.class, args);
 	}
 
 }
--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudstorageApplicationTests.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudstorageApplicationTests.java
@@ -1,10 +1,10 @@
-package com.udacity.jdnd.course1.cloudstorage;
+package com.udacity.jwdnd.course1.cloudstorage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CloudStorageApplicationTests {
+class CloudstorageApplicationTests {
 
 	@Test
 	void contextLoads() {
