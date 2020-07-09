626ad3bff878795b544aae0f2e7479aae31c0ae0
awalakaushik <kawala@uh.edu>
2020-07-08 22:49:04 -0500

    Add file model

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/models/File.java
@@ -0,0 +1,60 @@
+package com.udacity.jwdnd.course1.cloudstorage.models;
+
+import java.sql.Blob;
+
+public class File {
+    private Integer fileId;
+    private String fileName;
+    private String contentType;
+    private String fileSize;
+    private Integer userId;
+    private Blob fileData;
+
+    public Integer getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(Integer fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Blob getFileData() {
+        return fileData;
+    }
+
+    public void setFileData(Blob fileData) {
+        this.fileData = fileData;
+    }
+}
