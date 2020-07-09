7798815d463e1dbf5334edecf94847e2f7ef056e
awalakaushik <kawala@uh.edu>
2020-07-08 22:48:57 -0500

    Add credential model

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/models/Credential.java
@@ -0,0 +1,58 @@
+package com.udacity.jwdnd.course1.cloudstorage.models;
+
+public class Credential {
+    private int credentialId;
+    private String url;
+    private String username;
+    private String key;
+    private String password;
+    private int userId;
+
+    public int getCredentialId() {
+        return credentialId;
+    }
+
+    public void setCredentialId(int credentialId) {
+        this.credentialId = credentialId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
