dd67394028cbd623434e4560cebac34635bc36b9
awalakaushik <kawala@uh.edu>
2020-07-08 22:49:11 -0500

    Add note model

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/models/Note.java
@@ -0,0 +1,40 @@
+package com.udacity.jwdnd.course1.cloudstorage.models;
+
+public class Note {
+    private Integer noteId;
+    private String noteTitle;
+    private String noteDescription;
+    private Integer userId;
+
+    public Integer getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(Integer noteId) {
+        this.noteId = noteId;
+    }
+
+    public String getNoteTitle() {
+        return noteTitle;
+    }
+
+    public void setNoteTitle(String noteTitle) {
+        this.noteTitle = noteTitle;
+    }
+
+    public String getNoteDescription() {
+        return noteDescription;
+    }
+
+    public void setNoteDescription(String noteDescription) {
+        this.noteDescription = noteDescription;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
