874cd8e6bc5a53ca100c7f2e0131bd4b477c0ff5
KAUSHIK REDDY AWALA <kawala@uh.edu>
2020-07-10 21:11:33 -0500

    Add controllers (#4)
    * Add Home Controller
    
    * Add Login Controller
    
    * Add Signup Controller

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.udacity.jwdnd.course1.cloudstorage.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping
+    public String getHomePage() {
+        return "home";
+    }
+}
---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LoginController.java
@@ -0,0 +1,15 @@
+package com.udacity.jwdnd.course1.cloudstorage.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping
+    public String getLoginPage() {
+        return "login";
+    }
+}
