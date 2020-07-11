ce440a00b73072546b9708f4b5b48517a52b1ea7
awalakaushik <kawala@uh.edu>
2020-07-10 22:43:10 -0500

    Correct sql query syntax error

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
@@ -8,12 +8,11 @@ public interface UserMapper {
     @Select("SELECT * FROM Users WHERE username = #{username}")
     User getUser(String username);
 
-    @Insert("INSERT INTO Users(username, salt, password, firstName,lastName) VALUES (#{username}, " +
-            "${salt}," +
-            " #{password}, #{firstName}, #{lastName})")
-    @Options(useGeneratedKeys = true, keyProperty = "userId")
+    @Insert("INSERT INTO Users(username, salt, password, firstname, lastname) VALUES (#{username}, #{salt}, " +
+            "#{password}, #{firstName}, #{lastName})")
+    @Options(useGeneratedKeys = true, keyProperty = "userid")
     Integer insertUser(User user);
 
-    @Delete("DELETE FROM Users WHERE userId = #{userId}")
+    @Delete("DELETE FROM Users WHERE userId = #{userid}")
     void deleteUser(Integer userId);
 }
