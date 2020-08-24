98936bed56e229090b2528cf7736c1c935d0cb91
samsond <nosmaspoise@gmail.com>
2020-08-24 19:30:29 +0300

    added login & signup test

---
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/LoginPage.java
@@ -0,0 +1,38 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    @FindBy(id = "inputUsername")
+    private WebElement usernameField;
+
+    @FindBy(id = "inputPassword")
+    private WebElement passwordField;
+
+    @FindBy(id = "submit-button")
+    private WebElement submitButton;
+
+    @FindBy(id = "error-msg")
+    private WebElement errorMessage;
+
+    @FindBy(id = "logout-msg")
+    private WebElement logoutMessage;
+
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void login(String username, String password) {
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+        submitButton.click();
+    }
+
+    public String getErrorMessage() {
+        return errorMessage.getText();
+    }
+}
