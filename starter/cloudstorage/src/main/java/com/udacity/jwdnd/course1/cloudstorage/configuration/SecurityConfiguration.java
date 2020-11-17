e09c645ea04705bdfcfba6b515026fce63594111
Athanasios Kourtzis <athkou@hotmail.com>
2020-11-17 15:58:16 +0100

    feat: Implement second configure method in the SecurityConfiguration class.
    Override the second configure method to override the default login page
    and to restrict unauthorized users to the login and signup pages.

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/configuration/SecurityConfiguration.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.udacity.jwdnd.course1.cloudstorage.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -19,5 +20,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) {
 		auth.authenticationProvider(authenticationService);
+	}
+	
+	@Override 
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		    .antMatchers("/signup", "/css/**", "/js/**")
+		    .permitAll()
+		    .anyRequest()
+		    .authenticated();
+		
+		http.formLogin()
+		    .loginPage("/login")
+		    .permitAll();
+		
+		http.formLogin()
+		    .defaultSuccessUrl("/home", true);
+		
+		http.logout()
+	    	.logoutSuccessUrl("/login")
+	    	.invalidateHttpSession(true)
+	    	.deleteCookies("JSESSIONID");
 	}
 }
