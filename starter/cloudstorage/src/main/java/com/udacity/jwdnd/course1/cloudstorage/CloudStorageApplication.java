baaa37a8dd871ffaa60bf9f3c38d604bde2f99ef
serendipiri <seren.piri@gmail.com>
2021-07-06 00:19:27 +0300

    first commit test

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
@@ -8,6 +8,7 @@ public class CloudStorageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudStorageApplication.class, args);
+		System.out.println("dsfsd");
 	}
 
 }
