29612b1634da9d75c2802651c5c7ec6b928ae735
Safwat <ahmedsafwatsultan@hotmail.com>
2021-10-18 19:14:09 +0200

    Overriding default springboot security configuration with an empty one

---
+++ starter/cloudstorage/src/main/java/safwat/cloudstorage/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package safwat.cloudstorage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import safwat.cloudstorage.services.AuthenticationService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	AuthenticationService authenticationService;
+	
+	public SecurityConfig(AuthenticationService authenticationService) {
+		// TODO Auto-generated constructor stub
+		this.authenticationService = authenticationService;
+	}
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		//super.configure(auth);
+		auth.authenticationProvider(this.authenticationService);
+	}
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		//super.configure(http);
+
+		
+	}
+}
---
+++ starter/cloudstorage/src/main/java/safwat/cloudstorage/services/AuthenticationService.java
@@ -0,0 +1,22 @@
+package safwat.cloudstorage.services;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements AuthenticationProvider {
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
