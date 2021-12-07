47102772d63de251d13c11ab22baeb9a5d0570da
Farhan Ahmad <farhan@udacity.com>
2021-12-07 18:49:26 +0530

    Add unit tests for invalid URL and large file uploads.

--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -2,11 +2,17 @@ package com.udacity.jwdnd.course1.cloudstorage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
+import java.io.File;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudStorageApplicationTests {
 
@@ -37,5 +43,120 @@ class CloudStorageApplicationTests {
 		driver.get("http://localhost:" + this.port + "/login");
 		Assertions.assertEquals("Login", driver.getTitle());
 	}
+
+	private void doMockSignUp(String firstName, String lastName, String userName, String password){
+		// Create a dummy account for logging in later.
+		WebDriverWait webDriverWait = new WebDriverWait(driver, 2);
+		driver.get("http://localhost:" + this.port + "/signup");
+		webDriverWait.until(ExpectedConditions.titleContains("Sign Up"));
+		
+		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputFirstName")));
+		WebElement inputFirstName = driver.findElement(By.id("inputFirstName"));
+		inputFirstName.click();
+		inputFirstName.sendKeys(firstName);
+
+		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputLastName")));
+		WebElement inputLastName = driver.findElement(By.id("inputLastName"));
+		inputLastName.click();
+		inputLastName.sendKeys(lastName);
+
+		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputUsername")));
+		WebElement inputUsername = driver.findElement(By.id("inputUsername"));
+		inputUsername.click();
+		inputUsername.sendKeys(userName);
+
+		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputPassword")));
+		WebElement inputPassword = driver.findElement(By.id("inputPassword"));
+		inputPassword.click();
+		inputPassword.sendKeys(password);
+
+		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("buttonSignUp")));
+		WebElement buttonSignUp = driver.findElement(By.id("buttonSignUp"));
+		buttonSignUp.click();
+
+		Assertions.assertEquals(driver.findElement(By.id("success-msg")).getText(), "You successfully signed up! Please continue to the login page.");
+
+
+		// Log in to our dummy account.
+		driver.get("http://localhost:" + this.port + "/login");
+		
+		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputUsername")));
+		WebElement loginUserName = driver.findElement(By.id("inputUsername"));
+		loginUserName.click();
+		loginUserName.sendKeys(userName);
+
+		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputPassword")));
+		WebElement loginPassword = driver.findElement(By.id("inputPassword"));
+		loginPassword.click();
+		loginPassword.sendKeys(password);
+
+		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login-button")));
+		WebElement loginButton = driver.findElement(By.id("login-button"));
+		loginButton.click();
+
+		webDriverWait.until(ExpectedConditions.titleContains("Home"));
+
+	}
+
+	/**
+	 * PLEASE DO NOT DELETE THIS TEST. You may modify this test to work with the 
+	 * rest of your code. 
+	 * This test is provided by Udacity to perform some basic sanity testing of 
+	 * your code to ensure that it meets certain rubric criteria. 
+	 * 
+	 * If this test is failing, please ensure that you are handling bad URLs 
+	 * gracefully, for example with a custom error page.
+	 * 
+	 * Read more about custom error pages at: 
+	 * https://attacomsian.com/blog/spring-boot-custom-error-page#displaying-custom-error-page
+	 */
+	@Test
+	public void testBadUrl() {
+		// Create a test account
+		doMockSignUp("URL","Test","UT","123");
+		
+		// Try to access a random made-up URL.
+		driver.get("http://localhost:" + this.port + "/some-random-page");
+		Assertions.assertFalse(driver.getPageSource().contains("Whitelabel Error Page"));
+	}
+
+
+	/**
+	 * PLEASE DO NOT DELETE THIS TEST. You may modify this test to work with the 
+	 * rest of your code. 
+	 * This test is provided by Udacity to perform some basic sanity testing of 
+	 * your code to ensure that it meets certain rubric criteria. 
+	 * 
+	 * If this test is failing, please ensure that you are handling uploading large files (>1MB),
+	 * gracefully in your code. 
+	 * 
+	 * Read more about file size limits here: 
+	 * https://spring.io/guides/gs/uploading-files/ under the "Tuning File Upload Limits" section.
+	 */
+	@Test
+	public void testLargeUpload() {
+		// Create a test account
+		doMockSignUp("Large File","Test","LFT","123");
+
+		// Try to upload an arbitrary large file
+		WebDriverWait webDriverWait = new WebDriverWait(driver, 2);
+		String fileName = "upload5m.zip";
+
+		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("fileUpload")));
+		WebElement fileSelectButton = driver.findElement(By.id("fileUpload"));
+		fileSelectButton.sendKeys(new File(fileName).getAbsolutePath());
+
+		WebElement uploadButton = driver.findElement(By.id("uploadButton"));
+		uploadButton.click();
+		try {
+			webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("success")));
+		} catch (org.openqa.selenium.TimeoutException e) {
+			System.out.println("Large File upload failed");
+		}
+		Assertions.assertFalse(driver.getPageSource().contains("HTTP Status 403 – Forbidden"));
+
+	}
+
+
 
 }
