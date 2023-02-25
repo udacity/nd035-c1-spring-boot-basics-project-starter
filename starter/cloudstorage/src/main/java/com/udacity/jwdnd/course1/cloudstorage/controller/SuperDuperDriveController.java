c46545c851b9acc54672ff33227e1d06f2f3b480
PANDAREVELUV-PC\PandaReveluv <dat19071998@gmail.com>
2023-02-25 22:29:24 +0700

    implement success mess flow

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SuperDuperDriveController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SuperDuperDriveController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -45,38 +46,60 @@ public class SuperDuperDriveController {
     }
 
     @PostMapping(path = "/note")
-    public String addNote(Model model, @ModelAttribute Note note) {
+    public String addNote(RedirectAttributes redirectAttributes, Model model, @ModelAttribute Note note) {
 
         if (note.getNoteId() != null) {
             boolean isNoteUpdated = noteService.updateNote(note) != 0;
+            buildSuccessAttributes(redirectAttributes, "Note updated successfully");
         } else {
             boolean isNoteAdded = noteService.addNote(note) != 0;
+            buildSuccessAttributes(redirectAttributes, "Note added successfully");
         }
         return "redirect:/home";
     }
 
     @GetMapping(path = "/note/{noteId}/delete")
-    public String deleteNote(Model model, @PathVariable("noteId") String noteId) {
+    public String deleteNote(RedirectAttributes redirectAttributes, Model model, @PathVariable("noteId") String noteId) {
 
         boolean isNoteDeleted = noteService.deleteNote(Integer.parseInt(noteId)) != 0;
+        if (isNoteDeleted) {
+            buildSuccessAttributes(redirectAttributes, "Note deleted successfully");
+        } else {
+            buildErrorAttributes(redirectAttributes, "Can not delete note");
+        }
         return "redirect:/home";
     }
 
     @PostMapping(path = "/credential")
-    public String addCredential(Model model, @ModelAttribute Credential credential) {
+    public String addCredential(RedirectAttributes redirectAttributes, Model model, @ModelAttribute Credential credential) {
 
         if (credential.getCredentialId() != null) {
             boolean isCredentialUpdated = credentialService.updateCredential(credential) != 0;
+            if (isCredentialUpdated) {
+                buildSuccessAttributes(redirectAttributes, "Credential updated successfully");
+            } else {
+                buildErrorAttributes(redirectAttributes, "Can not update note");
+            }
         } else {
             boolean isCredentialAdded = credentialService.addCredential(credential) != 0;
+            if (isCredentialAdded) {
+                buildSuccessAttributes(redirectAttributes, "Credential added successfully");
+            } else {
+                buildErrorAttributes(redirectAttributes, "Can not add credential");
+            }
         }
         return "redirect:/home";
     }
 
     @GetMapping(path = "/credential/{credentialId}/delete")
-    public String deleteCredential(Model model, @PathVariable("credentialId") String credentialId) {
+    public String deleteCredential(RedirectAttributes redirectAttributes, Model model, @PathVariable("credentialId") String credentialId) {
 
         boolean isCredentialDeleted = credentialService.deleteCredential(Integer.parseInt(credentialId)) != 0;
+        if (isCredentialDeleted) {
+            buildSuccessAttributes(redirectAttributes, "Credential deleted successfully");
+        } else {
+            buildErrorAttributes(redirectAttributes, "Can not delete credential");
+        }
         return "redirect:/home";
     }
 
@@ -89,9 +112,15 @@ public class SuperDuperDriveController {
         file.setFileSize(String.valueOf(fileUpload.getSize()));
         InputStream inputStream = fileUpload.getInputStream();
         file.setFileData(inputStream.readAllBytes());
-        boolean isFileAdded = fileService.addFile(file) != 0;
-        if (!isFileAdded) {
-            buildErrorAttributes(redirectAttributes, "Can not upload file, duplicate File Name");
+        if (StringUtils.isEmpty(file.getFileName()) || file.getFileData().length == 0) {
+            buildErrorAttributes(redirectAttributes, "There is no file to upload");
+        } else {
+            boolean isFileAdded = fileService.addFile(file) != 0;
+            if (isFileAdded) {
+                buildSuccessAttributes(redirectAttributes, "File added successfully");
+            } else {
+                buildErrorAttributes(redirectAttributes, "Can not upload file, duplicate File Name");
+            }
         }
         return "redirect:/home";
     }
@@ -105,14 +134,24 @@ public class SuperDuperDriveController {
     }
 
     @GetMapping(path = "/file/{fileId}/delete")
-    public String deleteFile(Model model, @PathVariable("fileId") String fileId) {
+    public String deleteFile(RedirectAttributes redirectAttributes, Model model, @PathVariable("fileId") String fileId) {
 
         boolean isFileDeleted = fileService.deleteFile(Integer.parseInt(fileId)) != 0;
+        if (isFileDeleted) {
+            buildSuccessAttributes(redirectAttributes, "File deleted successfully");
+        } else {
+            buildErrorAttributes(redirectAttributes, "Can not delete file");
+        }
         return "redirect:/home";
     }
 
-    private void buildErrorAttributes(RedirectAttributes redirectAttributes, String errorMesss) {
+    private void buildErrorAttributes(RedirectAttributes redirectAttributes, String errorMess) {
         redirectAttributes.addFlashAttribute("isError", true);
-        redirectAttributes.addFlashAttribute("errorMess", errorMesss);
+        redirectAttributes.addFlashAttribute("errorMess", errorMess);
+    }
+
+    private void buildSuccessAttributes(RedirectAttributes redirectAttributes, String successMess) {
+        redirectAttributes.addFlashAttribute("isSuccess", true);
+        redirectAttributes.addFlashAttribute("successMess", successMess);
     }
 }
